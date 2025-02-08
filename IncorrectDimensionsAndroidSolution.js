eceafe42da91076ffb68833927b3184655e0cf44
BugHunter-X <askzenai@gmail.com>
2025-02-09 00:58:06 +0100

    Add IncorrectDimensionsAndroidSolution.js file

---
+++ IncorrectDimensionsAndroidSolution.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+
+const App = () => {
+  const [dimensions, setDimensions] = useState(Dimensions.get('window'));
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener('change', ({ window }) => {
+      setDimensions(window);
+    });
+    return () => subscription?.remove();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Window Width: {dimensions.width}</Text>
+      <Text>Window Height: {dimensions.height}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
